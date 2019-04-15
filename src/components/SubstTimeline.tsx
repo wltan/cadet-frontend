80a5a80f146dc044fbf2d93ce51d84f5f2bfd9f8
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-04-15 13:47:42 +0800

    Add buttons to SUBST timeline to allow for fine-grained control over stepping through substitution

--- src/components/SubstTimeline.tsx
+++ src/components/SubstTimeline.tsx
@@ -12,9 +12,16 @@ export class SubstTimeline extends React.PureComponent<ISubstTimelineProps, ISub
   constructor(props : ISubstTimelineProps) {
     super(props);
     this.trees = props.trees;
-    this.sliderChanged = this.sliderChanged.bind(this);
+    
     this.updateTrees = this.updateTrees.bind(this);
     this.getFinalValue = this.getFinalValue.bind(this);
+    
+    this.sliderChanged = this.sliderChanged.bind(this);
+    this.sliderShift = this.sliderShift.bind(this);
+    this.stepFirst = this.stepFirst.bind(this);
+    this.stepPrev = this.stepPrev.bind(this);
+    this.stepNext = this.stepNext.bind(this);
+    this.stepLast = this.stepLast.bind(this);
   }
 
   public componentDidMount() {
@@ -33,7 +40,13 @@ export class SubstTimeline extends React.PureComponent<ISubstTimelineProps, ISub
             : "Start writing some code on the left, then drag the slider below to see it's evaluation."
           }
         </div>
-        <input ref={x=>this.slider=x} id="substSlider" type="range" min="0" max={this.trees? this.trees.length-1 : 0} defaultValue="0" onChange={this.sliderChanged}/>
+        <span>
+          <button onClick={this.stepFirst}>{"|<"}</button>
+          <button onClick={this.stepPrev}>{"<"}</button>
+          <button onClick={this.stepNext}>{">"}</button>
+          <button onClick={this.stepLast}>{">|"}</button>
+          <input ref={x=>this.slider=x} id="substSlider" type="range" min="0" max={this.trees? this.trees.length-1 : 0} defaultValue="0" onChange={this.sliderChanged}/>
+        </span>
       </div>
     );
   }
@@ -74,8 +87,38 @@ export class SubstTimeline extends React.PureComponent<ISubstTimelineProps, ISub
 
   private sliderChanged(event : React.ChangeEvent<HTMLInputElement>) {
 
-    const sliderValue = parseInt(event.target.value, 10);
-    this.setState({value: sliderValue, trees: this.state.trees});
+    const newValue = parseInt(event.target.value, 10);
+    this.setState({value: newValue, trees: this.state.trees});
+  }
+
+  private sliderShift(newValue : number) {
+
+    if (this.slider) {
+
+      if (newValue < 0 || newValue > parseInt(this.slider.max, 10)) {
+        alert("Slider is already at most extreme value.");
+      }
+      else {
+        this.slider.value = newValue.toString();
+        this.setState({value: newValue, trees: this.state.trees});
+      }
+    }
+  }
+
+  private stepFirst() {
+    this.sliderShift(0);
+  }
+
+  private stepLast() {
+    this.sliderShift(this.trees ? this.trees.length - 1 : 0);
+  }
+
+  private stepPrev() {
+    this.sliderShift(this.slider ? parseInt(this.slider.value, 10) - 1 : 0);
+  }
+
+  private stepNext() {
+    this.sliderShift(this.slider ? parseInt(this.slider.value, 10) + 1 : 0);
   }
 
   private generateFromTree(tree : es.Node) : string {
