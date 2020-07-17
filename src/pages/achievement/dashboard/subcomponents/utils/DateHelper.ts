ce22950f2936603ba8efbfc110e9e1118f2e6231
Jet Kan <jetkan.yk@gmail.com>
2020-07-17 10:41:17 +0800

    Completed prereq indicator & shorten tag
    (cherry picked from commit d16bf46cb8d804f26d9fe84a7657fadacc0fdfb0)

--- src/pages/achievement/dashboard/subcomponents/utils/DateHelper.ts
+++ src/pages/achievement/dashboard/subcomponents/utils/DateHelper.ts
@@ -112,7 +112,7 @@ export const prettifyDeadline = (deadline: Date | undefined, now: Date) => {
   } else if (hoursAway > 1) {
     prettifiedDeadline = hoursAway + ' Hours';
   } else {
-    prettifiedDeadline = 'Less than 1 hour';
+    prettifiedDeadline = '1 Hour';
   }
 
   return prettifiedDeadline;
